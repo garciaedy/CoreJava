bb72972c86b625fee0770b1f34c6148e970a4bb9
garciaedy <edygarcia97@gmail.com>
2024-01-02 23:14:54 -0500

    created inheritance package.. using shape for example

---
+++ src/inheritance/Shape.java
@@ -0,0 +1,13 @@
+package inheritance;
+
+public class Shape {
+    private string name;
+
+    public string getName() {
+        return name;
+    }
+
+    public void setName(string name) {
+        this.name = name;
+    }
+}
