0f1dc3dc8e0f99c8036d1aca4b5552725b2cacaa
edygarcia97 <edygarcia97@gmail.com>
2025-02-03 21:50:36 -0500

    practice

--- src/lesson_303_5_iteration_statements_loops/Lesson_303_5_Iteration_Statements_Loops.java
+++ src/lesson_303_5_iteration_statements_loops/Lesson_303_5_Iteration_Statements_Loops.java
@@ -3,5 +3,10 @@ package lesson_303_5_iteration_statements_loops;
 public class Lesson_303_5_Iteration_Statements_Loops {
     public static void main(String[] args){
 
+        for(int step =1; step<=10; step++){
+            System.out.println("Taking steps: " + step);
+        }
+
+
     }
 }
---
+++ src/lesson_303_5_iteration_statements_loops/PatternWscanner.java
@@ -0,0 +1,2 @@
+package lesson_303_5_iteration_statements_loops;public class PatternWscanner {
+}
