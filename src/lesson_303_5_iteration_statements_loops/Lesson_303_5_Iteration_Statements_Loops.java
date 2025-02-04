dd0a359466dbb1f1db9d3799461644f6098ddb65
edygarcia97 <edygarcia97@gmail.com>
2025-02-04 16:40:16 -0500

    loops practice

--- src/lesson_303_5_iteration_statements_loops/Lesson_303_5_Iteration_Statements_Loops.java
+++ src/lesson_303_5_iteration_statements_loops/Lesson_303_5_Iteration_Statements_Loops.java
@@ -6,10 +6,79 @@ public class Lesson_303_5_Iteration_Statements_Loops {
 //        for(int step =1; step<=10; step++){
 //            System.out.println("Taking steps: " + step);
 //        }
-        for(int number = 0; number<=10; number++){
-            System.out.println("counting: " + number);
+//        for(int number = 0; number<=10; number++){
+//            System.out.println("counting: " + number);
+//
+//}
+//        for(int backnum =15; backnum >=0; backnum--){
+//            System.out.println("countdown: " + backnum);
+//        }
+//
+//        for(int evenNum =0; evenNum<=20; evenNum+=2){
+//            System.out.println("Even number" + evenNum);
+//        }
+//        for(int evenNum=0; evenNum<=30; evenNum++){
+//            if(evenNum %2 ==0){
+//                System.out.println("Even number: " + evenNum);
+//            }
+//        }
 
-}
+//        for(int oddNum =1; oddNum<=30; oddNum+=2){
+//            System.out.println("odd Number: " + oddNum);
+//        }
+
+//        for(int num=0; num<=20; num++){
+//            if(num %2 !=0){
+//                System.out.println("odd number: " + num );
+//            }
+//        }
+
+//        for(int multNum =0; multNum <=30; multNum+=5 ){
+//            System.out.println("Multiple of 5: " + multNum);
+//        }
+//
+
+//        int number =10;
+//
+//        System.out.println("Multiplication Table of " + number);
+//        for(int num =0; num<=10; num++ ){
+//            System.out.println(number + " X " + num + " = " +(number * num) );
+//        }
+
+//        int number1 =3;
+//        System.out.println("Number to add : " + number1);
+//        int results=0;
+//        for(int i=0;  i<20; i++){
+//            results+=number1;
+//            System.out.println(number1 +" After iteration " + (i + 1) + ": " + results);
+//
+//        }
+
+//    int n =10;
+//        for(int i =0; i<=n;i++){
+//            System.out.println("int" +i);
+//        }
+//        for (int i = 0, j = 10; i <= j; i++, j--) {
+//            System.out.println("i: " + i + ", j: " + j);
+//        }
+//       for(int i=1; i<=10; i++){
+//           for(int j=0; j<=2; j++){
+//               System.out.print(i+ " - " + j + " " );
+//           }
+//        System.out.println();
+//       }
+
+//        for(int i=1; i<=5; i++){
+//            for(int j=0; j<=2; j++){
+//                System.out.print("* ");
+//            }
+//            System.out.println();
+//        }
+
+        for(int i =0; i<=10; i++){
+            System.out.print("* ");
+        }
+        System.out.println();
 
     }
 }
