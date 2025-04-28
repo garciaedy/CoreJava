beddc938ec7fea3c9d3e893fb399412f63083ad0
edygarcia97 <edygarcia97@gmail.com>
2025-04-28 15:36:21 -0400

    practice

--- src/lesson_303_4_conditional_statements_and_flow_control/Conditionals.java
+++ src/lesson_303_4_conditional_statements_and_flow_control/Conditionals.java
@@ -9,43 +9,100 @@ public class Conditionals {
 //            System.out.println("The number is negative");
 //        }
 
+////        System.out.println("");
+//        int number1 = 23;
+//        if(number1 % 2 ==0){
+//            System.out.println("The number is even");
+//        }else{
+//            System.out.println("The number is odd");
+//        }
+//
 //        System.out.println("");
-        int number1 = 23;
-        if(number1 % 2 ==0){
-            System.out.println("The number is even");
+
+//
+//        int num2= 13;
+//        if(num2 %2 ==0){
+//            System.out.println("it's divisible");
+//        }else{
+//            System.out.println("it's not divisible");
+//        }
+/*
+        int temperatureF =85;
+
+        if(temperatureF >=85){
+            System.out.println("it's very hot");
+        }else if (temperatureF >=70){
+            System.out.println("it's warm");
+
+        }else if(temperatureF >=62){
+            System.out.println("it's pleasant");
         }else{
-            System.out.println("The number is odd");
+            System.out.println("It's cold");
         }
+*/
+       /* int starRating =5;
 
-        System.out.println("");
-//        System.out.println("");
-//        System.out.println("");
-//        System.out.println("");
-//        System.out.println("");
-//        System.out.println("");
-//        System.out.println("");
-//        System.out.println("");
-//        System.out.println("");
-//        System.out.println("");
-//        System.out.println("");
-//        System.out.println("");
-//        System.out.println("");
-//        System.out.println("");
-//        System.out.println("");
-//        System.out.println("");
-//        System.out.println("");
-//        System.out.println("");
-//        System.out.println("");
-//        System.out.println("");
-//        System.out.println("");
-//        System.out.println("");
-//        System.out.println("");
-//        System.out.println("");
-//        System.out.println("");
-//        System.out.println("");
-//        System.out.println("");
+        if(starRating == 5){
+            System.out.println("it's a ");
+        }else if(starRating == 4){
+            System.out.println("it's b");
+        }else if(starRating == 3){
+            System.out.println("its c");
+        }else {
+            System.out.println("it aint good.");
+        }*/
+
+         /* int temperatureF = 45;
+        if(temperatureF >= 90){
+            System.out.println("It's very hot");
+        }else if(temperatureF >=80){
+            System.out.println("it's hot");
+        }else if(temperatureF >= 70){
+            System.out.println("It's warm");
+        }else if(temperatureF >= 60){
+            System.out.println("It's chill");
+        }else if(temperatureF >=50){
+            System.out.println("its cold");
+        }else {
+            System.out.println("it's very cold");
+        }
+       */
 
 
+
+        /*
+        //BMICheck
+
+        * */
+
+        /* */
+        /* */
+        /* */
+        /* */
+        /* */
+        /* */
+        /* */
+        /* */
+        /* */
+        /* */
+        /* */
+        /* */
+        /* */
+        /* */
+        /* */
+        /* */
+        /* */
+        /* */
+        /* */
+        /* */
+        /* */
+        /* */
+        /* */
+        /* */
+        /* */
+        /* */
+        /* */
+        /* */
 
     }
 }
